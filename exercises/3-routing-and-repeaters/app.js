ce13d7926936352efc247ebb126f1deaf7dff28c
Randy Eppinger <Randy.Eppinger@leandog.com>
2015-05-20 14:17:33 -0400

    Finished Exercise 3 routes and repeaters.

--- exercises/3-routing-and-repeaters/app.js
+++ exercises/3-routing-and-repeaters/app.js
@@ -1,5 +1,17 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngRoute']);
 
 // controller here
+app.controller('FirstController', function($scope) {
+  $scope.stooges = ['larry', 'curly', 'moe']
+});
 
 // routes here
+app.config(function($routeProvider) {
+	$routeProvider.when('/stooges', {
+		controller: 'FirstController',
+		templateUrl: 'stooges.html'
+	});
+	// $routeProvider.otherwise({
+	// 	redirectTo: '/stooges'
+	// })
+})
