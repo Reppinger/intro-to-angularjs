60688f0efc0e9f9c68c01773e801fc898fd5cd5e
Randy Eppinger <Randy.Eppinger@leandog.com>
2015-05-20 14:28:57 -0400

    Renamed controller to stooges.

--- exercises/3-routing-and-repeaters/app.js
+++ exercises/3-routing-and-repeaters/app.js
@@ -1,14 +1,14 @@
 var app = angular.module('app', ['ngRoute']);
 
 // controller here
-app.controller('FirstController', function($scope) {
+app.controller('StoogeController', function($scope) {
   $scope.stooges = ['larry', 'curly', 'moe']
 });
 
 // routes here
 app.config(function($routeProvider) {
 	$routeProvider.when('/stooges', {
-		controller: 'FirstController',
+		controller: 'StoogeController', //'StoogeController as stooge'
 		templateUrl: 'stooges.html'
 	});
 	// $routeProvider.otherwise({
